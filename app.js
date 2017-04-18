7494cc84693637a24f49e2f776b441a5dfc64882
Daniel Hernán Vargas Osorio <dhernan.vargas@gmail.com>
2017-04-18 16:54:24 -0500

    fix 1

--- app.js
+++ app.js
@@ -22,11 +22,70 @@ var estados = [];
   // var vars = query.split("?");
   var vars = getParameterByName("indicador");
   console.log(vars);
+  var obj = getParameterByName("objetivo");
+  var meta = getParameterByName("meta");
   var codigoDg = getParameterByName("codigo");
   console.log(codigoDg);
   var PCveInd = vars;
   var Codigo_ind = '';
   var Descrip_ind = '';
+  var tituloObjetivo = '';
+
+  $.ajax({
+    type: 'POST',
+    url: "https://operativos.inegi.org.mx/datos/api/Tematica/Todos",
+    data: {'PIdioma':'ES'},
+    success: function( data, textStatus, jqxhr ) {
+    		//alert( "Exito" );
+  		// console.log(data);
+  		// console.log('------------------------------------- nuevo arreglo  --------------------');
+  		// console.log(data.Series);
+  		// console.log(data.Series[0]);
+  		// console.log(data.Series[0].Coberturas);
+  		// console.log(data.Series[0].Coberturas.length);
+
+      nombreObj = data[0].Abrevia_des;
+      console.log(obj+nombreObj);
+  		Codigo_ind 	=	data.Codigo_ind;
+      Descrip_ind = data.Descrip_ind;
+
+
+  		var temporal = [];
+  		temporal.push('Entidad');
+  		for (var j = 0; j < data.Series[0].Coberturas[0].ValorDato.length; j++) {
+  			temporal.push(data.Series[0].Coberturas[0].ValorDato[j].AADato_ser+'-01-01');
+  		}
+  		estados.push(temporal);
+
+
+  		for (var i = 0; i < data.Series[0].Coberturas.length; i++) {
+  			var temporal = [];
+  			temporal.push(data.Series[0].Coberturas[i].Descrip_cg);
+  			for (var j = 0; j < data.Series[0].Coberturas[i].ValorDato.length; j++) {
+  				temporal.push(data.Series[0].Coberturas[i].ValorDato[j].Dato_ser);
+  			}
+  			estados.push(temporal);
+  		}
+
+  		// console.log(estados);
+
+  		var codigo_indicador = data.Codigo_ind;
+  		// console.log(codigo_indicador);
+  		var descripcion = data.Descrip_ind;
+  		// console.log(descripcion);
+      $('.tituloObjetivo').html(obj+nombreObj);
+  		$('.Codigo_ind').html(Codigo_ind);
+  		$('.Descrip_ind').html(Descrip_ind);
+  		//alert(Descrip_ind);
+
+  		//inicio =  1;
+  		$('#loader').delay(2000).fadeOut("slow");
+  		titulos(PCveInd);
+    },
+    async:false
+  });
+
+
 
 $.ajax({
   type: 'POST',
@@ -34,15 +93,15 @@ $.ajax({
   data: {'PCveInd': PCveInd,'PAnoIni':'0', 'PAnoFin':'0', 'POrden':'DESC', 'PIdioma':'ES'},
   success: function( data, textStatus, jqxhr ) {
   		//alert( "Exito" );
-		console.log(data);
-		console.log('------------------------------------- nuevo arreglo  --------------------');
-		console.log(data.Series);
-		console.log(data.Series[0]);
-		console.log(data.Series[0].Coberturas);
-		console.log(data.Series[0].Coberturas.length);
+		// console.log(data);
+		// console.log('------------------------------------- nuevo arreglo  --------------------');
+		// console.log(data.Series);
+		// console.log(data.Series[0]);
+		// console.log(data.Series[0].Coberturas);
+		// console.log(data.Series[0].Coberturas.length);
 
 		Codigo_ind 	=	data.Codigo_ind;
-  		Descrip_ind = 	data.Descrip_ind;
+    Descrip_ind = data.Descrip_ind;
 
 
 		var temporal = [];
@@ -62,12 +121,12 @@ $.ajax({
 			estados.push(temporal);
 		}
 
-		console.log(estados);
+		// console.log(estados);
 
 		var codigo_indicador = data.Codigo_ind;
-		console.log(codigo_indicador);
+		// console.log(codigo_indicador);
 		var descripcion = data.Descrip_ind;
-		console.log(descripcion);
+		// console.log(descripcion);
 
 		$('.Codigo_ind').html(Codigo_ind);
 		$('.Descrip_ind').html(Descrip_ind);
