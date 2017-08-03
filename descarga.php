e5d9a7f6c4a1db87eeacc5b8472a35613c959603
Daniel Hernán Vargas Osorio <dhernan.vargas@gmail.com>
2017-08-03 12:54:22 -0500

    Algoritmos

--- descarga.php
+++ descarga.php
@@ -1,3 +1,33 @@
+<?php
+
+// When executed in a browser, this script will prompt for download
+// of 'test.xls' which can then be opened by Excel or OpenOffice.
+
+require 'lib/php-export-data.class.php';
+
+// 'browser' tells the library to stream the data directly to the browser.
+// other options are 'file' or 'string'
+// 'test.xls' is the filename that the browser will use when attempting to
+// save the download
+$exporter = new ExportDataExcel('browser', 'IndicadorDM.xls');
+
+$exporter->initialize(); // starts streaming data to web browser
+
+// pass addRow() an array and it converts it to Excel XML format and sends
+// it to the browser
+$exporter->addRow(array("This"));
+$exporter->addRow(array(1, 2, 3, "123-456-7890"));
+
+// doesn't care how many columns you give it
+$exporter->addRow(array("foo"));
+
+$exporter->finalize(); // writes the footer, flushes remaining data to browser.
+
+exit(); // all done
+
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
